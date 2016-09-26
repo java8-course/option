8018e44529b2594a7942f05d622208fbabc4a851
Semen Popugaev <Semen_Popugaev@epam.com>
2016-09-26 15:56:21 +0300

    lesson 5

---
+++ src/test/java/option/Option.java
@@ -0,0 +1,21 @@
+package option;
+
+public class Option<T> {
+    static <T> Option<T> empty() {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    static <T> Option<T> of(T t) {
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+
+    // filter
+    // map
+    // flatMap
+    // forEach
+    // isEmpty
+    // orElse
+    // getOrElse
+
+}
