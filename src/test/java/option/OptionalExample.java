f93e8f527ddffa1abd00924b55243c8afa117111
Semen Popugaev <Semen_Popugaev@epam.com>
2016-09-26 17:12:22 +0300

    ex

---
+++ src/test/java/option/OptionalExample.java
@@ -0,0 +1,37 @@
+package option;
+
+import org.junit.Test;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class OptionalExample {
+
+    @Test
+    public void get() {
+        final Optional<String> o1 = Optional.empty();
+
+        o1.ifPresent(s -> System.out.println(s));
+
+        o1.orElse("t");
+        o1.orElseGet(() -> "t");
+        o1.orElseThrow(() -> new UnsupportedOperationException());
+    }
+
+    @Test
+    public void ifPresent() {
+        final Optional<String> o1 = getOptional();
+
+        o1.ifPresent(System.out::println);
+
+        if (o1.isPresent()) {
+            System.out.println(o1.get());
+        }
+    }
+
+    private Optional<String> getOptional() {
+        return ThreadLocalRandom.current().nextBoolean()
+            ? Optional.empty()
+            : Optional.of("abc");
+    }
+}
