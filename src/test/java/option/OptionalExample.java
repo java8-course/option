593a65fd1ba5cd77aa15634f433b65fabcfc48e1
mnesvit <nesvit.maria@gmail.com>
2016-12-20 12:07:46 +0300

    hw optional

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,7 +19,7 @@ public class OptionalExample {
 
         o1.orElse("t");
         o1.orElseGet(() -> "t");
-        o1.orElseThrow(() -> new UnsupportedOperationException());
+//        o1.orElseThrow(() -> new UnsupportedOperationException());
     }
 
     @Test
@@ -46,13 +47,58 @@ public class OptionalExample {
         } else {
             actual = Optional.empty();
         }
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void filter() {
+        final Optional<String> o1 = getOptional();
+        final Optional<String> actual;
+        final Predicate<String> p = s -> s.contains("a");
+        final Optional<String> expected = o1.filter(p);
+
+        if (o1.isPresent()) {
+            if (p.test(o1.get())) {
+                actual = o1;
+            } else {
+                actual = Optional.empty();
+            }
+        } else {
+            actual = Optional.empty();
+        }
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void flatMap() {
+        final Optional<String> o1 = getOptional();
+        final Optional<String> actual;
+        final Function<String, Optional<String>> replace = s -> (s == null) ? Optional.empty() : Optional.of(s.replace("abc", "cba"));
+        final Optional<String> expected = o1.flatMap(replace);
+        if (o1.isPresent())
+            actual = replace.apply(o1.get());
+        else
+            actual = Optional.empty();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void orElse() {
+        final Optional<String> o1 = getOptional();
+        final String actual;
+        final String defaultVal = "xyz";
+        final String expected = o1.orElse(defaultVal);
+        if (o1.isPresent()) {
+            actual = o1.get();
+        } else {
+            actual = defaultVal;
+        }
         assertEquals(expected, actual);
     }
 
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean()
-            ? Optional.empty()
-            : Optional.of("abc");
+                ? Optional.empty()
+                : Optional.of("abc");
     }
 }
