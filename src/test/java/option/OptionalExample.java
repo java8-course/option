90c04c93c0c75ec789a828651f4cb5d1a1c6e2d6
Aleksandr Gribkov <Aleksandr_Gribkov@epam.com>
2017-01-08 00:40:21 +0300

    To do optional

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -46,7 +47,43 @@ public class OptionalExample {
         } else {
             actual = Optional.empty();
         }
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void orElse() {
+        final Optional<String> o1 = getOptional();
+        final String s = "default value";
+        final String expected = o1.orElse(s);
+        final String actual;
+        if(o1.isPresent()) actual = o1.get();
+        else actual = s;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void filter() {
+        final Optional<String> o1 = getOptional();
+        final Predicate<String> predicate = s->s.isEmpty();
+        final Optional<String> expected = o1.filter(predicate);
+        final Optional<String> actual;
+
+        if(o1.isPresent()) {
+            if (predicate.test(o1.get())) actual = o1;
+            else actual = o1.empty();
+        }
+        else actual = o1.empty();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void flatMap() {
+        final Optional<String> o1 = getOptional();
+        final Function<String, Optional<String>> stringOptionalFunction = s -> Optional.of(s.substring(1));
+        final Optional<String> expected = o1.flatMap(stringOptionalFunction);
+        final Optional<String> actual;
+        if(o1.isPresent()) actual = stringOptionalFunction.apply(o1.get());
+        else actual = Optional.empty();
         assertEquals(expected, actual);
     }
 
