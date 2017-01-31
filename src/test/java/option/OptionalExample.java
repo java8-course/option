bb5e30a8876673ee7ea01c39f293cc9feca373ef
Max Elkin <maxim-el@mail.ru>
2017-01-31 15:13:38 +0300

    Hometask #1 completed

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -1,10 +1,12 @@
 package option;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,7 +20,7 @@ public class OptionalExample {
 
         o1.orElse("t");
         o1.orElseGet(() -> "t");
-        o1.orElseThrow(() -> new UnsupportedOperationException());
+        //o1.orElseThrow(UnsupportedOperationException::new);
     }
 
     @Test
@@ -48,6 +50,47 @@ public class OptionalExample {
         }
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void filter(){
+        final Optional<String> o1 = getOptional();
+        final Predicate<String> pred = "abc"::equals;
+        final Optional<String> expected = o1.filter(pred);
+        final Optional<String> actual;
+        if (o1.isPresent()){
+            actual = pred.test(o1.get()) ? o1 : Optional.empty();
+        } else{
+            actual = Optional.empty();
+        }
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void flatMap(){
+        final Optional<String> o1 = getOptional();
+        final Function<String, Optional<String>> flatMapFunction = s -> Optional.of(String.valueOf(s.length()));
+        final Optional<String> expected = o1.flatMap(flatMapFunction);
+        final Optional<String> actual;
+        if (o1.isPresent()){
+            actual = flatMapFunction.apply(o1.get());
+        } else{
+            actual = Optional.empty();
+        }
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void orElse(){
+        final Optional<String> o1 = getOptional();
+        final String expected = o1.orElse("def");
+        final String actual;
+        if (o1.isPresent()){
+            actual = o1.get();
+        } else{
+            actual = "def";
+        }
+        Assert.assertEquals(expected,actual);
     }
 
     private Optional<String> getOptional() {
