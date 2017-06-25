eefc3d4bbd59d3cb332018110590f7a4e7962f4b
TimWinnicheck <vinnichekt@yandex.ru>
2017-06-25 16:05:09 +0300

    Added orElse

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -30,7 +30,7 @@ public class OptionalExample {
 
     @Test
     public void ifPresent() {
-        Optional<String> o1 = this.getOptional();
+        Optional<String> o1 = getOptional();
 
         o1.ifPresent(System.out::println);
 
@@ -41,7 +41,7 @@ public class OptionalExample {
 
     @Test
     public void map() {
-        Optional<String> o1 = this.getOptional();
+        Optional<String> o1 = getOptional();
 
         Function<String, Integer> getLength = String::length;
 
@@ -60,7 +60,7 @@ public class OptionalExample {
 
     @Test
     public void filter() {
-        Optional<String> opt = this.getOptional();
+        Optional<String> opt = getOptional();
 
         Predicate<String> test = s -> s.equals("fffff");
 
@@ -82,7 +82,7 @@ public class OptionalExample {
     @Test
     public void flatMap() {
 
-        Optional<String> opt = getOptional();
+        Optional<String> opt = this.getOptional();
 
         Function<String, Optional<Integer>> getLength = s -> Optional.of(s.length());
 
@@ -97,6 +97,24 @@ public class OptionalExample {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void orElse() {
+
+        Optional<String> opt = this.getOptional();
+
+        String exp = "fffff";
+
+        String expected = opt.orElse(exp);
+
+        String actual;
+
+        if (opt.isPresent()) {
+            actual = opt.get();
+        } else {
+            actual = exp;
+        }
+        assertEquals(expected, actual);
+    }
 
 
     private Optional<String> getOptional() {
