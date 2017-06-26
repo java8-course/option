5535c6d6aecfb87c7c0325210b206d48e6581cc5
Mikhail_Sidnev <Mikhail_Sidnev@epam.com>
2017-06-26 11:16:11 +0300

    + orElseGet + orElse + orElseTrow + filter + flatMap

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -2,10 +2,13 @@ package option;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class OptionalExample {
@@ -52,7 +55,105 @@ public class OptionalExample {
 
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean()
-            ? Optional.empty()
-            : Optional.of("abc");
+                ? Optional.empty()
+                : Optional.of("abc");
+    }
+
+    @Test
+    public void orElseGet() throws Exception {
+
+        Optional<String> optional = getOptional();
+
+        String other = "other";
+
+        String expected = optional.orElseGet(() -> other);
+
+        String actual = optional.isPresent() ? optional.get() : other;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void orElse() throws Exception {
+
+        Optional<String> optional = getOptional();
+
+        String other = "other";
+
+        String expected = optional.orElse(other);
+
+        String actual = optional.isPresent() ? optional.get() : other;
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void orElseTrow() throws Exception {
+        boolean isOptionalTrow = false;
+        String extended = "";
+
+        Optional<String> optional = getOptional();
+        try {
+            extended = optional.orElseThrow(NoSuchElementException::new);
+        } catch (NoSuchElementException e) {
+            isOptionalTrow = true;
+        }
+
+        boolean isImplementationThrow = false;
+
+        String actual = "";
+        try {
+            if (optional.isPresent()) {
+                actual = optional.get();
+            } else {
+                throw new NoSuchElementException();
+            }
+        } catch (NoSuchElementException e) {
+            isImplementationThrow = true;
+        }
+
+
+        assertEquals(isOptionalTrow, isImplementationThrow);
+
+        if (!isOptionalTrow)
+            assertEquals(extended, actual);
+
+    }
+
+    @Test
+    public void filter() throws Exception {
+        Optional<String> optional = getOptional();
+
+        String s = "abc";
+        Optional<String> abc = optional.filter(Predicate.isEqual(s));
+
+        Optional<String> actual = optional.isPresent() ?
+                optional.get().equals(s) ?
+                        optional :
+                        Optional.empty() :
+                Optional.empty();
+
+        assertEquals(abc, actual);
+    }
+
+    @Test
+    public void flatMap() throws Exception {
+
+        Optional<String> optional = getOptional();
+
+        Optional<char[]> expected = optional.flatMap(s1 -> Optional.of(s1.toCharArray()));
+
+        Optional<char[]> actual = optional.isPresent() ?
+                Optional.of(optional.get().toCharArray()) :
+                Optional.empty();
+
+        if (expected.isPresent()) {
+            assertArrayEquals(expected.get(), actual.get());
+        } else {
+            assertEquals(expected, actual);
+        }
     }
 }
