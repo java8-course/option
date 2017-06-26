07f6e1c16b3f76c23835fcfed899929d1f5eb092
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-06-26 12:32:49 +0300

    + optional methods

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
 
@@ -50,9 +52,115 @@ public class OptionalExample {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void flatMap() {
+        final Optional<String> o1 = getOptional();
+
+        final Function<String, Optional<Integer>> getLength = s -> Optional.of(s.length());
+
+        final Optional<Integer> expected = o1.flatMap(getLength);
+
+        final Optional<Integer> actual;
+        if (o1.isPresent()) {
+            actual = getLength.apply(o1.get());
+        } else {
+            actual = Optional.empty();
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void filter() {
+        final Optional<String> o1 = getOptional();
+
+        final Predicate<String> getLength = s -> s.length() > 0;
+
+        final Optional<String> expected = o1.filter(getLength);
+
+        final Optional<String> actual;
+        if (o1.isPresent()) {
+            actual = getLength.test(o1.get()) ? o1 : Optional.empty();
+        } else {
+            actual = Optional.empty();
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void orElse() {
+        final Optional<String> o1 = getOptional();
+
+        String qqq = "qqq";
+
+        final Optional<String> expected = Optional.of(o1.orElse(qqq));
+
+        final Optional<String> actual;
+        if (o1.isPresent()) {
+            actual = Optional.of(o1.get());
+        } else {
+            actual = Optional.of(qqq);
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void orElseGet() {
+        final Optional<String> o1 = getOptional();
+
+        String qqq = "qqq";
+        Supplier<String> stringSupplier = () -> qqq;
+
+        final Optional<String> expected = Optional.of(o1.orElseGet(stringSupplier));
+
+        final Optional<String> actual;
+        if (o1.isPresent()) {
+            actual = Optional.of(o1.get());
+        } else {
+            actual = Optional.of(stringSupplier.get());
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void orElseThrow() {
+        final Optional<String> o1 = getOptional();
+        boolean flag = false;
+        boolean flag2 = false;
+
+        Supplier<NullPointerException> exceptionSupplier = NullPointerException::new;
+        String expected = "";
+        String actual = "";
+
+        try {
+            expected = o1.orElseThrow(exceptionSupplier);
+        } catch (NullPointerException e) {
+            flag = true;
+        }
+
+        try {
+            if (o1.isPresent()) {
+                actual = o1.get();
+            } else {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            flag2 = true;
+        }
+        assertEquals(flag, flag2);
+
+        if (!flag) {
+            assertEquals(expected, actual);
+        }
+    }
+
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean()
-            ? Optional.empty()
-            : Optional.of("abc");
+                ? Optional.empty()
+                : Optional.of("abc");
     }
 }
+
