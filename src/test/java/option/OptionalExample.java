b09a52e8694c7f3cfb0909d47df71384f5b55548
Narek Karapetyan <narek.27.02.93@mail.ru>
2017-07-07 20:09:36 +0300

    Part 4 initial

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -2,22 +2,40 @@ package option;
 
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 
 public class OptionalExample {
 
+    public static <T1, T2, R> Optional<R> zipMap(Optional<T1> o1, Optional<T2> o2, BiFunction<T1, T2, R> f){
+        // isPresent, get none
+        //use map + flatMap
+        Optional<T1> oo1 = o1.flatMap(Optional::ofNullable);
+        Optional<T2> oo2 = o2.flatMap(Optional::ofNullable);
+
+//        f.apply(oo1, oo2);
+        return null;
+    }
+
     @Test
     public void get() {
         final Optional<String> o1 = Optional.empty();
 
-        o1.ifPresent(s -> System.out.println(s));
+        o1.ifPresent(s -> assertEquals("abc", s));
 
-        o1.orElse("t");
-        o1.orElseGet(() -> "t");
+        assertEquals("t", o1.orElse("t"));
+        assertEquals("t", o1.orElseGet(() -> "t"));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void get2() {
+        final Optional<String> o1 = Optional.empty();
+
         o1.orElseThrow(() -> new UnsupportedOperationException());
     }
 
@@ -25,10 +43,10 @@ public class OptionalExample {
     public void ifPresent() {
         final Optional<String> o1 = getOptional();
 
-        o1.ifPresent(System.out::println);
+        o1.ifPresent(s -> assertEquals("abc", s));
 
         if (o1.isPresent()) {
-            System.out.println(o1.get());
+            assertEquals("abc", o1.get());
         }
     }
 
@@ -50,9 +68,58 @@ public class OptionalExample {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void flatMap() {
+        final Optional<String> o1 = getOptional();
+
+        final Optional<Integer> expected = o1.flatMap(s -> Optional.of(s.length()));
+
+        final Optional<Integer> actual;
+        if (o1.isPresent()) {
+            actual = Optional.of(o1.get().length());
+        } else {
+            actual = Optional.empty();
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void filter() {
+        final Optional<String> o1 = getOptional();
+
+        o1.filter(s -> s.length() >= 3)
+                .ifPresent(s -> assertEquals("abc", s));
+
+        final Optional<String> actual;
+        if (o1.isPresent()) {
+            actual = Optional.of(o1.get()).filter(s -> s.length() >= 3);
+            assertEquals("abc", actual.get());
+        } else {
+            actual = Optional.empty();
+        }
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void filter2() {
+        final Optional<String> o1 = getOptional();
+
+        o1.filter(s -> s.length() >= 4)
+                .ifPresent(System.out::println);
+
+        final Optional<String> actual;
+        if (o1.isPresent()) {
+            actual = Optional.of(o1.get()).filter(s -> s.length() >= 4);
+            assertEquals("abc", actual.get());
+        } else {
+            actual = Optional.empty();
+            actual.get();
+        }
+    }
+
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean()
-            ? Optional.empty()
-            : Optional.of("abc");
+                ? Optional.empty()
+                : Optional.of("abc");
     }
 }
