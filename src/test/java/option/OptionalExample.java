a189cf52b5c37a86d9b0951fa6efd8e645c5174e
Nikita Golubev <nikita_golubev@epam.com>
2017-07-11 18:45:00 +0300

    part1 done

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,7 +13,7 @@ public class OptionalExample {
 
     @Test
     public void get() {
-        final Optional<String> o1 = Optional.empty();
+        final Optional<String> o1 = getOptional();
 
         o1.ifPresent(s -> System.out.println(s));
 
@@ -50,9 +51,66 @@ public class OptionalExample {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void flatMap() {
+        final Optional<String> optional = getOptional();
+
+        final Function<String, Optional<Integer>> getLength = s -> Optional.of(s.length());
+
+        final Optional<Integer> expected = optional.flatMap(getLength);
+
+        final Optional<Integer> actual;
+
+        if (optional.isPresent()) {
+            actual = getLength.apply(optional.get());
+        } else {
+            actual = Optional.empty();
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void filter() {
+        final Optional<String> optional = getOptional();
+
+        final Predicate<String> predicate = s -> s.contains("ab");
+
+        final Optional<String> expected = optional.filter(predicate);
+
+        final Optional<String> actual;
+
+        if (optional.isPresent() && predicate.test(optional.get())) {
+            actual = optional;
+        } else {
+            actual = Optional.empty();
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void orElse() {
+        final Optional<String> optional = getOptional();
+
+        final String param = "ABC";
+
+        final String expected = optional.orElse(param);
+
+        final String actual;
+
+        if (optional.isPresent()) {
+            actual = optional.get();
+        } else {
+            actual = param;
+        }
+
+        assertEquals(expected, actual);
+    }
+
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean()
-            ? Optional.empty()
-            : Optional.of("abc");
+                ? Optional.empty()
+                : Optional.of("abc");
     }
 }
