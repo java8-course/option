5e0697c43febfe313e07feb8c9015611d6b07230
Vasilii Bobkov <vasily-bobkow@yandex.ru>
2017-07-13 18:20:51 +0300

    from lab

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -1,14 +1,25 @@
 package option;
 
+import com.sun.istack.internal.Nullable;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 public class OptionalExample {
+
+    private final String TEST_VALUE_1 = "TEST_VALUE_1";
+    private final String TEST_VALUE_2 = "TEST_VALUE_2";
 
     @Test
     public void get() {
@@ -50,9 +61,72 @@ public class OptionalExample {
         assertEquals(expected, actual);
     }
 
+    @Test(expected = NoSuchElementException.class)
+    public void orELseThrow1() {
+        Optional<String> optional = Optional.empty();
+        optional.orElseThrow(() -> new NoSuchElementException("empty value"));
+    }
+
+    @Test
+    public void orELseThrow2() {
+        Optional<String> optional = Optional.of(TEST_VALUE_1);
+        String fromOptional = optional.orElseThrow(() -> new NoSuchElementException("empty value"));
+        assertThat(fromOptional, is(TEST_VALUE_1));
+    }
+
+    @Test
+    public void flatMap() {
+        class TesterFlatMap{
+            @Nullable
+            private String field;
+            Optional<String> getField(){
+                return Optional.ofNullable(field);
+            }
+
+            void setField(String field) {
+                this.field = field;
+            }
+        }
+        TesterFlatMap testerFlatMap = new TesterFlatMap();
+        testerFlatMap.setField(TEST_VALUE_1);
+        assertTrue(Optional.of(testerFlatMap)
+                .flatMap(TesterFlatMap::getField)
+                .isPresent());
+    }
+
+    @Test
+    public void tet(){
+        getOptional().orElseGet(s-> new )
+    }
+
+    // TODO: 7/7/2017 написать несколько тестов, которые должны быть с get и function
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean()
-            ? Optional.empty()
-            : Optional.of("abc");
+                ? Optional.empty()
+                : Optional.of("abc");
+    }
+
+    @Test
+    public void filter() {
+        Optional<String> optional_1 = Optional.of(TEST_VALUE_1);
+        optional_1 = optional_1.filter(s -> s.startsWith("J"));
+        assertFalse(optional_1.isPresent());
+
+        Optional<String> optional_2 = Optional.of(TEST_VALUE_2);
+        optional_2 = optional_2.filter(s->s.startsWith("T"));
+        assertTrue(optional_2.isPresent());
+
+
+    }
+
+    @Test
+    public void testZipMap(){
+
+    }
+    // TODO: 7/7/2017 написание метода zip для коллекции не использовать get и ifPresent (Можно использовать map и flatMap).
+    // Ну и написать тесты на это днище. И да, без мутабельных переменных.
+
+    public static <T1, T2, R> Optional<R> zipMap(Optional<T1> o1, Optional<T2> o2, BiFunction<T1, T2, R> f) {
+        return o1.flatMap(obj1 -> o2.map(obj2 -> f.apply(obj1, obj2)));
     }
 }
