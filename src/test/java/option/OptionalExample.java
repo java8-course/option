ede8555c8b399c97ebcde0b6a9d678c353a4c72d
Olya Lee <kimidake@yandex.ru>
2017-07-26 09:27:19 +0300

    Optional homework (Olga Li)

--- src/test/java/option/OptionalExample.java
+++ src/test/java/option/OptionalExample.java
@@ -10,6 +10,9 @@ import static org.junit.Assert.assertEquals;
 
 public class OptionalExample {
 
+    private static final String other = "other";
+    private static final String ABC = "abc";
+
     @Test
     public void get() {
         final Optional<String> o1 = Optional.empty();
@@ -50,9 +53,65 @@ public class OptionalExample {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void orElse() {
+        Optional<String> o1 = getOptional();
+
+        String expected = o1.orElse(other);
+
+        String actual = o1.isPresent() ? o1.get() : other;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void orElseGet() {
+        Optional<String> o1 = getOptional();
+
+        String expected = o1.orElseGet(() -> other);
+
+        String actual = o1.isPresent() ? o1.get() : other;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void filter() {
+        Optional<String> o1 = getOptional();
+
+        Optional<String> expected = Optional.of(ABC);
+
+        Optional<String> actual = o1.filter(s -> s.equals(ABC));
+
+        if (!o1.isPresent()) {
+            expected = Optional.empty();
+        }
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void flatMap() {
+        Optional<String> o1 = getOptional();
+
+        Function<String, Optional<String>> firstChar = s -> Optional.of(s.substring(0, 1));
+
+        Optional<String> expected = o1.flatMap(firstChar);
+
+        Optional<String> actual;
+
+        if (o1.isPresent()) {
+            actual = firstChar.apply(o1.get());
+        } else {
+            actual = Optional.empty();
+        }
+
+        assertEquals(expected, actual);
+    }
+
     private Optional<String> getOptional() {
         return ThreadLocalRandom.current().nextBoolean()
-            ? Optional.empty()
-            : Optional.of("abc");
+                ? Optional.empty()
+                : Optional.of(ABC);
     }
 }
